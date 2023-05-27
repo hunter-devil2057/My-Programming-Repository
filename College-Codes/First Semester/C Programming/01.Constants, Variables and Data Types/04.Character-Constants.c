47cd5cec614c650a85bba13d42ce8f27a40dfccf
hunter-devil2057 <manishshiwakoti42@gmail.com>
2023-05-27 17:30:09 +0545

    Understanding Character Constants in C

---
+++ College-Codes/First Semester/C Programming/01.Constants, Variables and Data Types/04.Character-Constants.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n=4;
+    char c='4';
+    n=n+4;      //n=4+5=9
+    c=c+5;      //c=(ASCII Value of 4)+5=57
+    printf("Integer: %d", n);
+    printf("\nCharacter:%d", c);
+}
