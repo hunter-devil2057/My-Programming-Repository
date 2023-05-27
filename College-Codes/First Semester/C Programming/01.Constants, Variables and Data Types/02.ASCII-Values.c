22a0ed158196365fec6cf537ec73484d45c1dee8
hunter-devil2057 <manishshiwakoti42@gmail.com>
2023-05-27 16:49:58 +0545

    Displaying ASCII Values of Various Characters in C

---
+++ College-Codes/First Semester/C Programming/01.Constants, Variables and Data Types/02.ASCII-Values.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+    a='A';
+    b='B';
+    c='C';
+    d='D';
+    e='E';
+    f='F';
+    g='G';
+    h='H';
+    i='I';
+    j='J';
+    k='K';
+    l='L';
+    m='M';
+    n='N';
+    o='O';
+    p='P';
+    q='Q';
+    r='R';
+    s='S';
+    t='T';
+    u='U';
+    v='V';
+    w='W';
+    x='X';
+    y='Y';
+    z='Z';
+    printf("|\tCharacter\t|\tASCII Value\t|");
+    printf("\n|_______________________|_______________________|");
+    printf("\n|\t%c\t\t|\t\t%d\t|", a, a);
+    printf("\n|\t%c\t\t|\t\t%d\t|", b, b);
+    printf("\n|\t%c\t\t|\t\t%d\t|", c, c);
+    printf("\n|\t%c\t\t|\t\t%d\t|", d, d);
+    printf("\n|\t%c\t\t|\t\t%d\t|", e, e);
+    printf("\n|\t%c\t\t|\t\t%d\t|", f, f);
+    printf("\n|\t%c\t\t|\t\t%d\t|", g, g);
+    printf("\n|\t%c\t\t|\t\t%d\t|", h, h);
+    printf("\n|\t%c\t\t|\t\t%d\t|", i, i);
+    printf("\n|\t%c\t\t|\t\t%d\t|", j, j);
+    printf("\n|\t%c\t\t|\t\t%d\t|", k, k);
+    printf("\n|\t%c\t\t|\t\t%d\t|", l, l);
+    printf("\n|\t%c\t\t|\t\t%d\t|", m, m);
+    printf("\n|\t%c\t\t|\t\t%d\t|", n, n);
+    printf("\n|\t%c\t\t|\t\t%d\t|", o, o);
+    printf("\n|\t%c\t\t|\t\t%d\t|", p, p);
+    printf("\n|\t%c\t\t|\t\t%d\t|", q, q);
+    printf("\n|\t%c\t\t|\t\t%d\t|", r, r);
+    printf("\n|\t%c\t\t|\t\t%d\t|", s, s);
+    printf("\n|\t%c\t\t|\t\t%d\t|", t, t);
+    printf("\n|\t%c\t\t|\t\t%d\t|", u, u);
+    printf("\n|\t%c\t\t|\t\t%d\t|", v, v);
+    printf("\n|\t%c\t\t|\t\t%d\t|", w, w);
+    printf("\n|\t%c\t\t|\t\t%d\t|", x, x);
+    printf("\n|\t%c\t\t|\t\t%d\t|", y, y);
+    printf("\n|\t%c\t\t|\t\t%d\t|", z, z);
+    printf("\n|_______________________|_______________________|");
+
+    /*
+        Here, \n breaks the line
+    */
+
+}
