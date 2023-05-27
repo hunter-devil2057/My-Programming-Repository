810961038e836dda237f422c1f2c1c34c3e946ef
hunter-devil2057 <manishshiwakoti42@gmail.com>
2023-05-27 18:48:51 +0545

    Displaying the Product of Two Numbers

---
+++ College-Codes/First Semester/C Programming/01.Constants, Variables and Data Types/Classwork/Classwork4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    float firstNum, secondNum, result;
+    printf("Enter First Number:\n");
+    scanf("%f", &firstNum);     //receiving first number.
+    printf("Enter Second Number:\n");
+    scanf("%f", &secondNum);        //receiving second number...
+    // 
+    result=firstNum*secondNum;      //performing addition operation. 
+    // Displaying result...
+    printf("Therefore, the product of two numbers is:\n\t%.2f + %.2f = %.2f", firstNum, secondNum, result);
+    getch();
+    return 0;
+}
