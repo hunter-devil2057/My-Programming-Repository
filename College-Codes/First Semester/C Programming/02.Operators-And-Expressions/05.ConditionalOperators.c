8a71049ff6ab4b0e798154458eb1dfc768eeee91
hunter-devil2057 <manishshiwakoti42@gmail.com>
2023-05-28 22:41:25 +0545

    Understanding Conditional Operators in C

---
+++ College-Codes/First Semester/C Programming/02.Operators-And-Expressions/05.ConditionalOperators.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int firstNumber, secondNumber;
+    printf("Enter First Number:\n");
+    scanf("%d", &firstNumber);
+    printf("Enter Second Number:\n");
+    scanf("%d", &secondNumber);
+    // Operator pair '?;' is called as Conditional Operators 
+    // takes three operators so, called ternary operators.
+    /*
+    Syntax for Conditional Operators:
+        expression1 ? expression2 : expression3
+        Here, expression1 is evaluated first 
+        if expression1 appears as true then the value of expression2 is the value of conditional expression.
+        if expression1 appears as false then the value of expression3 is the value of conditional expression.
+    */ 
+   int larger=firstNumber>secondNumber?firstNumber:secondNumber;
+   printf("Therefore, the larger number is: %d", larger);
+   getch();
+   return 0;
+}
