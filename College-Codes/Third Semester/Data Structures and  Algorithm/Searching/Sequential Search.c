ada9607ab7eb7dc1088233ccb40cc62b45706b42
hunter-devil2057 <manishshiwakoti42@gmail.com>
2023-06-01 21:41:37 +0545

    Implementation of Sequential Search in C

---
+++ College-Codes/Third Semester/Data Structures and  Algorithm/Searching/Sequential Search.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a[100], key, i, n;
+    printf("Enter the number of elements in an array:\n");
+    scanf("%d", &n);
+    printf("Enter %d numbers:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter the number to search:\n");
+    scanf("%d", &key);
+    for (i=0; i<n; i++)
+    {
+        if (a[i]==key)
+        {
+            printf("%d is present at location %d .\n", key, i+1);
+            break;
+        }
+    }
+    if (i==n)
+    {
+        printf("%d is not present in an array.\n", key);
+    }
+    return 0;
+    
+    
+}
