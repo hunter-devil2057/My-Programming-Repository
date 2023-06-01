33a768c9f79f4a044e5a6246c9b7a727a40a4432
hunter-devil2057 <manishshiwakoti42@gmail.com>
2023-06-01 21:23:22 +0545

    Implementation of Bubble Sort in C

---
+++ College-Codes/Third Semester/Data Structures and  Algorithm/Sorting/01. Bubble_Sorting.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a[50],n,i,j,temp;
+    printf("Enter the number of Data item:\n");
+    scanf("%d",&n);
+    printf("Enter the elements:\n");
+    for(i=0;i<n;++i)
+        scanf("%d",&a[i]);
+    for(i=1;i<n;++i)
+        for(j=0;j<(n-i);++j)
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+    printf("\n Data after sorting: ");
+    for(i=0;i<n;++i)
+        printf("%d ",a[i]);
+    return 0;
+}
