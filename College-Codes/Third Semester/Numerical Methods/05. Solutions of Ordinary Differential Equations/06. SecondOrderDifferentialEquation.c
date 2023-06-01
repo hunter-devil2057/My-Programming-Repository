146161d7fbd679e5b94d0d078043d1792972ded7
hunter-devil2057 <manishshiwakoti42@gmail.com>
2023-06-01 19:07:11 +0545

    Implementation of Second ODE Using C

---
+++ College-Codes/Third Semester/Numerical Methods/05. Solutions of Ordinary Differential Equations/06. SecondOrderDifferentialEquation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+#define f1(x, y, z) z
+#define f2(x, y, z) -6*x+3*y-2*z
+
+int main()
+{
+    float x, xp, x0, y0, z0, m1y, m2y, m1z, m2z, h;
+    
+}
