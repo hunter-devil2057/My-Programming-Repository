ba29ac93b4ac5546531c354b89a68ea2f842cd56
hunter-devil2057 <manishshiwakoti42@gmail.com>
2023-06-05 17:10:38 +0545

    Understanding Escape Sequences, Printing Function & Comments in Java

---
+++ Self-Learning/Java-Learning/Basics/01.HelloWorld (Displaying Characters)/Main.java
@@ -0,0 +1,24 @@
+// This is a single line commment, ignored by compiler
+/*
+This is multi-line comment, ignored by compiler. 
+*/ 
+public class Main   //class name is always same as file name. 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+        System.out.println("I love pizzas and I find it really tasty");
+        System.out.println("It's really good......");
+        System.out.println("Learning Java is quite challening.\n\"You can develop android applications, games, and many more.\"\t[2]");
+        /*
+            Escape Sequences are the commands which requires opened and closed backslash to perform certain task.
+            like \" "\: is used to insert quoted marks 
+            \n: used to break line/ start a new line. 
+            \t: provides horizontal tab space 
+         * 
+        */
+
+    }
+}
+// While running/executing this file, a separate file named Main.class is created 
+// Main.class is a byte code file.
