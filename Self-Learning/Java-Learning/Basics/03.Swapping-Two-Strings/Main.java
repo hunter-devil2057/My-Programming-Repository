20087e8a4456e3fe9cb330b008a7fdab39fd4d2e
hunter-devil2057 <manishshiwakoti42@gmail.com>
2023-06-05 18:43:10 +0545

    Swapping Two Strings

---
+++ Self-Learning/Java-Learning/Basics/03.Swapping-Two-Strings/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Main
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String name1;
+        name1="Manish";
+        String name2="Anush";
+        System.out.println("Before Swapping:\nFirst Person: "+name1+"\nSecond Person:"+name2);
+        String temp=name1;
+        name1=name2;
+        name2=temp;
+        System.out.println("After Swapping:\nFirst Person: "+name1+"\nSecond Person: "+name2);
+    }
+}
