b1ff655edf3a7eba58c5e8bd83a24173bf783f42
hunter-devil2057 <manishshiwakoti42@gmail.com>
2023-07-12 18:26:55 +0545

    Finding Prefix, Suffix and Substring of String

---
+++ College-Codes/Fourth Semester/Theory of Computing/Deterministic-Finite-Automata/prefix-suffix.c
@@ -0,0 +1,71 @@
+// C program to find the prefix, suffix and substring
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void findPrefix(char string[]);
+void findSuffix(char string[]);
+void findSubstring(char string[], int, int);
+
+int main()
+{
+    char string[20];
+    int i, j;
+    printf("Enter a string:\n");
+    gets(string);
+
+    printf("\nPrefixes:");
+    findPrefix(string);
+    printf("\nSuffixes:");
+    findSuffix(string);
+
+    printf("Enter i and j for substring:\n");
+    scanf("%d%d", &i, &j);
+    findSubstring(string, i, j);
+    return 0;
+}
+
+void findPrefix(char string[])
+{
+    int i, j;
+    char prefix[20];
+    for (i=strlen(string); i>=0; i--)
+    {
+        for (j=0; j<i; j++)
+        {
+            prefix[j]=string[j];
+        }
+        prefix[j]='\0';
+        printf("\n%s", prefix);
+    }
+}
+
+void findSuffix(char string[])
+{
+    int i, j, k;
+    char suffix[20];
+    for (i=0; i<=strlen(string); i++)
+    {
+        k=i;
+        for (j=0; j<strlen(string); j++)
+        {
+            suffix[j]=string[k];
+            k++;
+        }
+        suffix[j]='\0';
+        printf("\n%s", suffix);
+    }
+}
+
+void findSubstring(char string[], int x, int y)
+{
+    char substring[20];
+    int k=0;
+    for (int i=x-1;i<y; i++)
+    {
+        substring[k]=string[i];
+        k++;
+    }
+    substring[k]='\0';
+    printf("Substring: %s", substring);
+}
