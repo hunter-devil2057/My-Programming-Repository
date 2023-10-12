4ceee777e75270264f01a9d865058e36828dce77
hunter-devil2057 <manishshiwakoti42@gmail.com>
2023-10-12 09:45:30 +0545

    Implementation of Vigenere Cipher in C

--- College-Codes/Fifth-Semester/Cryptography/02.Vigenere-Cipher.c
+++ College-Codes/Fifth-Semester/Cryptography/02.Vigenere-Cipher.c
@@ -3,88 +3,74 @@
 #include <string.h>
 #include <ctype.h>
 
-// Function to perform Vigenere Cipher encryption
-void vigenereEncrypt(char text[], char key[]) {
-    int textLength = strlen(text);
-    int keyLength = strlen(key);
-    for (int i = 0, j = 0; i < textLength; ++i) {
+void vigenereEncrypt(char text[], char key[]){
+    int i;
+    int textLen = strlen(text);
+    int keyLen = strlen(key);
+    char encrypted[textLen];
+    for (i = 0; i < textLen; i++) {
         char currentChar = text[i];
+        char keyChar = key[i % keyLen];
         if (isalpha(currentChar)) {
-            char keyChar = tolower(key[j % keyLength]) - 'a';
-            if (isupper(currentChar)) {
-                text[i] = (currentChar - 'A' + keyChar) % 26 + 'A';
-            } else {
-                text[i] = (currentChar - 'a' + keyChar) % 26 + 'a';
-            }
-            ++j;
+            char base = isupper(currentChar) ? 'A' : 'a';
+            encrypted[i] = (char)((currentChar + keyChar - 2 * base) % 26 + base);
+        } else {
+            encrypted[i] = currentChar; // Keep non-alphabetic characters unchanged
         }
     }
+    encrypted[i] = '\0';
+    
+    printf("Encrypted Text: %s\n", encrypted);
 }
 
-// Function to perform Vigenere Cipher decryption
-void vigenereDecrypt(char text[], char key[])
-{
-    int textLength = strlen(text);
-    int keyLength = strlen(key);
-    for (int i = 0, j = 0; i < textLength; ++i)
-    {
-        char currentChar = text[i];
-        if (isalpha(currentChar))
-        {
-            char keyChar = tolower(key[j % keyLength]) - 'a';
-            if (isupper(currentChar))
-            {
-                text[i] = (currentChar - 'A' - keyChar + 26) % 26 + 'A';
-            } else
-            {
-                text[i] = (currentChar - 'a' - keyChar + 26) % 26 + 'a';
-            }
-            ++j;
+void vigenereDecrypt(char encryptedText[], char key[]) {
+    int i;
+    int textLen = strlen(encryptedText);
+    int keyLen = strlen(key);
+    char decrypted[textLen];
+    
+    for (i = 0; i < textLen; i++) {
+        char currentChar = encryptedText[i];
+        char keyChar = key[i % keyLen];
+        
+        if (isalpha(currentChar)) {
+            char base = isupper(currentChar) ? 'A' : 'a';
+            decrypted[i] = (char)((currentChar - keyChar + 26) % 26 + base);
+        } else {
+            decrypted[i] = currentChar; // Keep non-alphabetic characters unchanged
         }
     }
+    decrypted[i] = '\0';
+    
+    printf("Decrypted Text: %s\n", decrypted);
 }
 
-int main()
-{
-    char text[1000];
+int main() {
+    char text[100];
     char key[100];
     int choice;
-
-    do
-    {
-        printf("Vigenere Cipher Menu:\n");
-        printf("1. Encrypt\n");
-        printf("2. Decrypt\n");
-        printf("3. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice)
-        {
-            case 1:
-                printf("Enter the text to be encrypted: ");
-                scanf(" %[^\n]s", text);
-                printf("Enter the key: ");
-                scanf("%s", key);
-                vigenereEncrypt(text, key);
-                printf("Encrypted text: %s\n", text);
-                break;
-            case 2:
-                printf("Enter the text to be decrypted: ");
-                scanf(" %[^\n]s", text);
-                printf("Enter the key: ");
-                scanf("%s", key);
-                vigenereDecrypt(text, key);
-                printf("Decrypted text: %s\n", text);
-                break;
-            case 3:
-                printf("Exiting...\n");
-                break;
-            default:
-                printf("Invalid choice. Please choose 1, 2, or 3.\n");
-                break;
-        }
-    } while (choice != 3);
-
+    
+    printf("Enter the text to be encrypted/decrypted:\n");
+    gets(text);
+    printf("Enter the key:\n");
+    gets(key);
+    
+    printf("Menu:\n");
+    printf("1. Encrypt Text\n");
+    printf("2. Decrypt Text\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    
+    switch(choice) {
+        case 1:
+            vigenereEncrypt(text, key);
+            break;
+        case 2:
+            vigenereDecrypt(text, key);
+            break;
+        default:
+            printf("Invalid Choice\n");
+    }
+    printf("Student's Name: Manish Shiwakoti\nT.U. Symbol No.: 26448/077");
     return 0;
 }
