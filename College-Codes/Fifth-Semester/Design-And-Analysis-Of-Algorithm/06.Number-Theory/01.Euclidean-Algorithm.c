c23cd3dff5bbcc45d0cda09fd27aa150d27f0bb0
hunter-devil2057 <manishshiwakoti42@gmail.com>
2024-01-08 22:30:44 +0545

    Implementing Euclidean-Algorithm in C

---
+++ College-Codes/Fifth-Semester/Design-And-Analysis-Of-Algorithm/06.Number-Theory/01.Euclidean-Algorithm.c
@@ -0,0 +1,27 @@
+// GCD using Euclidean algorithm
+#include <stdio.h>
+
+void gcd(int a, int b)
+{
+	if(a == 0) printf("%d is gcd", b);
+	else if(b == 0) printf("%d is gcd", a);
+	else
+	{
+		while(b != 0)
+		{
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		printf("%d is gcd", a);
+	}
+}
+int main()
+{
+	int n1, n2;
+	printf("Enter two positive integers: ");
+	scanf("%d %d",&n1,&n2);
+	gcd(n1, n2);
+	printf("\nName: Manish Shiwakoti\nT.U. Symbol No.: 26448/077\n");
+    return 0;
+}
