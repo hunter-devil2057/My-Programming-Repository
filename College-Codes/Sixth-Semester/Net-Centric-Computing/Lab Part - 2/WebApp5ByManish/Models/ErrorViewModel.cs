14d097b588e7997fbd48865a0cd7bbe85a49db70
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-07-19 09:43:00 +0545

    Delete College-Codes/Sixth-Semester/Net-Centric-Computing/Lab Part - 2 directory

--- College-Codes/Sixth-Semester/Net-Centric-Computing/Lab Part - 2/WebApp5ByManish/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApp5ByManish.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
